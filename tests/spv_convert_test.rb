51c50b5f5a4c59016d99f15fcd7a2446eb17d408
Tomek Drążek <tomek.drazek@gmail.com>
2016-10-12 11:41:27 +0200

    Basic test on page conversion with assetrtions

--- tests/spv_convert_test.rb
+++ tests/spv_convert_test.rb
@@ -18,35 +18,34 @@ class TestSPVConvert < Test::Unit::TestCase
   end
 
   def test_pdf_conversion
-    # page_11 = fixture_path("Olivia-2012-07-011.pdf")
-    _lorem = fixture_path('Lorem-ipsum.pdf')
-    _page = fixture_path('page.pdf')
-    # assert_true File.exists?(page_11)
-    # _convert_page(page_11, "./tmp/test/olivia-2012-07-011.tif", 1, {
-    #   "-sOutputICCProfile": fixture_path("ISOcoated_v2_eci.icc") } )
-    # _convert_page(lorem, "./tmp/test/lorem-2.tif", 2, {
-    # "-sOutputICCProfile": fixture_path("ISOcoated_v2_eci.icc") } )
-    # _convert_page(lorem, "./tmp/test/lorem-no-cms.tif", 2)
-
-    # cmyk = out.select{|k,v| [:cyan,:magenta,:yellow,:black].include?(k) }
-    # cmyk = SPV::Convert::CHANNELS.keys.map { |k| out[k] }
-    # puts cmyk
-
-    # out   = _convert_pdf_page("src/adv_TUI.pdf", "./tmp/test/tui_sep", 1)
-    # ch = _merge_channels(out, "./tmp/test/tui_ad")
-    # pp ch
-
     ch = Dir.mktmpdir do |tmp_dir|
       out = _convert_pdf_page(fixture_path('colors-spot.pdf'), tmp_dir, 1)
-      _merge_channels(out, './tmp/test/colors', "page-1")
+      _merge_channels(out, './tmp/colors-spot/page1', "001")
     end
-    pp ch
-
+    assert_equal 2, ch.count
+    assert_equal 3, ch[0][:channels].count
+    assert_true ch[0][:channels].include? :cyan
+    assert_true ch[0][:channels].include? :magenta
+    assert_true ch[0][:channels].include? :yellow
+    assert_true File.exists? ch[0][:img]
+    assert_equal 1, ch[1][:channels].count
+    assert_true ch[1][:channels].include? :black
+    assert_true File.exists? ch[1][:img]
     ch = Dir.mktmpdir do |tmp_dir|
       out = _convert_pdf_page(fixture_path('colors-spot.pdf'), tmp_dir, 2)
-      _merge_channels(out, './tmp/test/colors', "page-2")
+      _merge_channels(out, './tmp/colors-spot/page2', "001")
     end
-    pp ch
+    assert_equal 2, ch.count
+    assert_equal 3, ch[0][:channels].count
+    assert_true ch[0][:channels].include? :cyan
+    assert_true ch[0][:channels].include? :magenta
+    assert_true ch[0][:channels].include? :yellow
+    assert_true File.exists? ch[0][:img]
+    assert_equal 2, ch[1][:channels].count
+    assert_true ch[1][:channels].include? :black
+    assert_true ch[1][:channels].include? "Spot 1"
+    assert_true File.exists? ch[1][:img]
+    
     # _convert_page("./tests/fixtures/Olivia-2012-07-011.pdf", "./tmp/test/ol-01.tif", 1)
     # _convert_page(page, "./tmp/test/page-no-cms.tif", 1)
     # _convert_page(page, "./tmp/test/page.tif", 1, {
