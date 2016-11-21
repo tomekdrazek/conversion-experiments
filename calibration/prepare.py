21f645036e2fc3fe9b15d954c09ccae7cf44ac65
Tomek Drążek <tomek.drazek@gmail.com>
2016-11-21 15:31:59 +0100

    Refactoring: cleanup

--- calibration/prepare.py
+++
@@ -1,6 +0,0 @@
-import sys, math, time, subprocess
-# from subprocess import call, check_output
-
-# call(['ls', '-al'])
-
-out = subprocess.check_output(['ls', '-al'])
--- calibration/prepare.sh
+++
@@ -1,7 +0,0 @@
-for f in *.tiff; do
-  convert "$f" -fx C "$f.c.tif"
-  convert "$f" -fx M "$f.m.tif"
-  convert "$f" -fx Y "$f.y.tif"
-  convert "$f" -fx K "$f.k.jpg"
-  convert "$f.c.tif" "$f.m.tif" "$f.y.tif" -set colorspace RGB -combine "$f.cmy.jpg"
-done
