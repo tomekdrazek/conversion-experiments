71fe74a7ed89cd5ec3b4da763c4de0152d87a5a4
Tomek Drążek <tomek.drazek@me.com>
2016-12-07 15:53:37 +0100

    gemset changed for deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 server 'softproof2.newshubmedia.com', user: 'spv'
 
 set :rvm_type, :system
-set :rvm_ruby_version, '2.3.1@conversion-experiments'
+set :rvm_ruby_version, '2.3.1@spv'
 set :branch, 'master'
